2f0ff7745436399a8299d547fcd43d0ba611f944
Franz <fernandofranquiz@gmail.com>
2023-10-18 12:08:06 +0200

    changing the console.log for return in funtion

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,10 @@
 import { strictEquals } from "../scripts/tools.js";
-strictEquals(1, 1);
+/* StrictEquals(1, 1);
 strictEquals(false, false);
 strictEquals(0, -0);
 strictEquals(NaN, NaN);
+strictEquals(-0, 0); */
+
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(undefined, 2));
+console.log(strictEquals({}, {}));
--- scripts/tools.js
+++ scripts/tools.js
@@ -2,12 +2,17 @@ export const strictEquals = (a, b) => {
   const restOfResults = Object.is(a, b);
 
   if (Object.is(a, NaN) || Object.is(NaN, b)) {
-    console.log(false);
-  } else if (Object.is(a, -0) && Object.is(0, b)) {
-    console.log(true);
-  } else if (Object.is(-0, b) && Object.is(a, 0)) {
-    console.log(true);
-  } else {
-    console.log(restOfResults);
+    // Versión de Alenjandro:  if (isNan(a)) || isNan(b)){ return false;}
+    return false;
   }
+
+  if (Object.is(a, -0) && Object.is(0, b)) {
+    return true;
+  }
+
+  if (Object.is(-0, b) && Object.is(a, 0)) {
+    return true;
+  }
+
+  return restOfResults;
 };
